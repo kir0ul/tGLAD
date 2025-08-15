4826552ab3ffa018f2652b24d1992a9bdce08cb8
Andrea Pierré <andrea_pierre@student.uml.edu>
2025-08-15 23:09:04 +0000

    fix: out of memory + force create env

--- main.sh
+++ main.sh
@@ -3,8 +3,8 @@
 #SBATCH -G 1  # Number of GPUs
 #SBATCH --ntasks=8                   # Number of CPU cores
 #SBATCH --nodes=1                    # Number of nodes
-#SBATCH --mem=3gb                    # Job memory request
-#SBATCH --time=04:00:00              # Time limit hrs:min:sec
+#SBATCH --mem=16gb                    # Job memory request
+#SBATCH --time=08:00:00              # Time limit hrs:min:sec
 #SBATCH -o slurm-logs/%j-stdout.txt
 #SBATCH -e slurm-logs/%j-stderr.txt
 #SBATCH --mail-type=begin       # send email when job begins
@@ -17,7 +17,7 @@
 module load conda/latest
 conda config --add envs_dirs /work/pi_reza_azadeh_uml_edu/$USER/.conda/pkgs
 conda config --add pkgs_dirs /work/pi_reza_azadeh_uml_edu/$USER/.conda/envs
-mamba env create --file environment.yml --force --yes
+mamba env create --file environment.yml --yes
 conda activate tGLAD
 ## Run script
 python main.py
